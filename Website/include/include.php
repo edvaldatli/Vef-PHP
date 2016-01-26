00c6a38c9aef8f2ff68057e691bab5da818b00ba
edvaldatli <eddi1122@hotmail.com>
2016-01-26 15:13:17 +0000

    Bæti við Materialized
    Nav, footer

---
+++ Website/include/include.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0105982499
+ * Date: 26.1.2016
+ * Time: 14:13
+ */
+
+?>
+
+<head>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+</head>
+
+
+<nav>
+    <div class="nav-wrapper">
+        <div class="container">
+        <a href="#!" class="brand-logo">Logo</a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="sass.html"><i class="material-icons left">search</i>Link with Left Icon</a></li>
+            <li><a href="badges.html"><i class="material-icons right">view_module</i>Link with Right Icon</a></li>
+        </ul>
+        </div>
+    </div>
+</nav>
+    <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+<footer class="page-footer">
+    <div class="footer-copyright">
+        <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+        </div>
+    </div>
+</footer>
