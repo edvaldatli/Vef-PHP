d15d6786e808e3679d1f165eb8d2c725b0b29923
edvaldatli <eddi1122@hotmail.com>
2016-01-26 21:34:19 +0000

    Bæta við myndum og breyta stíl
    Navbar breyttur í svart.
    Betri mobile stuðningur

--- Website/include/include.php
+++ Website/include/include.php
@@ -13,6 +13,8 @@
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="css/style.css"/>
 
     <!--Let browser know website is optimized for mobile-->
@@ -20,26 +22,29 @@
 
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
 </head>
 
 
 <nav>
     <div class="nav-wrapper">
         <div class="container">
-        <a href="#!" class="brand-logo">Logo</a>
+        <a href="#!" class="brand-logo"><img src="img/logo.png"></a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-            <li><a href="sass.html"><i class="material-icons left">search</i>Link with Left Icon</a></li>
-            <li><a href="badges.html"><i class="material-icons right">view_module</i>Link with Right Icon</a></li>
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">Javascript</a></li>
+            <li><a href="mobile.html">Mobile</a></li>
         </ul>
         </div>
+        <ul class="side-nav" id="mobile-demo">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">Javascript</a></li>
+            <li><a href="mobile.html">Mobile</a></li>
+        </ul>
     </div>
 </nav>
     <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
-<footer class="page-footer">
-    <div class="footer-copyright">
-        <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-        </div>
-    </div>
-</footer>
+
--- Website/index.php
+++ Website/index.php
@@ -1,5 +1,5 @@
 <?php
-    include "include/include.php";
+include "include/include.php";
 
 ?>
 
@@ -8,25 +8,106 @@
 <head>
 
 </head>
-
-
 <body>
+    <div class="container row content">
 
-<div class="container row">
-    <div class="card col s2">
-        <div class="card-image waves-effect waves-block waves-light">
-            <img class="activator" src="http://www.download-free-wallpaper.com/img34/vilnzktfzymxnwckalms.jpg">
+        <div class="card col s12 m5">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="http://www.download-free-wallpaper.com/img34/vilnzktfzymxnwckalms.jpg">
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                <a href="#"><i class="material-icons">open_in_browser</i></a>
+            </div>
         </div>
-        <div class="card-content">
-            <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-            <p><a href="#">This is a link</a></p>
+
+        <div class="card col s12 m5 offset-m2">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="http://inspirationhut.net/wp-content/uploads/2013/06/unsplash-stock-photography-8.jpg">
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                <a href="#"><i class="material-icons">open_in_browser</i></a>
+            </div>
         </div>
-        <div class="card-reveal">
-            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+
+        <div class="card col s12 m5">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="http://28.media.tumblr.com/tumblr_lvyjcvriOb1r1irlvo1_500.jpg">
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                <a href="#"><i class="material-icons">open_in_browser</i></a>
+            </div>
         </div>
+
+        <div class="card col s12 m5 offset-m2">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="http://www.download-free-wallpaper.com/img34/vilnzktfzymxnwckalms.jpg">
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                <a href="#"><i class="material-icons">open_in_browser</i></a>
+            </div>
+        </div>
+
+        <div class="card col s12 m5">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="http://www.download-free-wallpaper.com/img34/vilnzktfzymxnwckalms.jpg">
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                <a href="#"><i class="material-icons">open_in_browser</i></a>
+            </div>
+        </div>
+
+        <div class="card col s12 m5 offset-m2">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="http://www.download-free-wallpaper.com/img34/vilnzktfzymxnwckalms.jpg">
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                <a href="#"><i class="material-icons">open_in_browser</i></a>
+            </div>
+        </div>
+
+        <div class="card col s12 m5">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="http://www.download-free-wallpaper.com/img34/vilnzktfzymxnwckalms.jpg">
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                <a href="#"><i class="material-icons">open_in_browser</i></a>
+            </div>
+        </div>
+
+        <div class="card col s12 m5 offset-m2">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="http://www.download-free-wallpaper.com/img34/vilnzktfzymxnwckalms.jpg">
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                <a href="#"><i class="material-icons">open_in_browser</i></a>
+            </div>
+        </div>
+
     </div>
-</div>
-</body>
 
+    <footer class="page-footer">
+        <div class="footer-copyright">
+            <div class="container">
+                © 2014 Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+        </div>
+    </footer>
+</body>
 </html>
