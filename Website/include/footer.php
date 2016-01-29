63c76b526f81c9c8073d17f9002ec3c3b6852fd6
edvaldatli <eddi1122@hotmail.com>
2016-01-29 02:14:38 +0000

    Update 1.3

--- Website/include/footer.php
+++ Website/include/footer.php
@@ -3,15 +3,14 @@
 ?>
 <html>
 <head>
-    <link type="text/css" rel="stylesheet" href="css/style.css" media="screen,projection"/>
 </head>
 
 <body>
     <footer class="page-footer">
         <div class="footer-copyright">
             <div class="container">
-                © 2014 Copyright Text
-                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                © 2016 Copyright Text
+                <span class="black-text text-lighten-4 right" ">Eðvald Atli</span>
             </div>
         </div>
     </footer>
--- Website/js/scripts.js
+++ Website/js/scripts.js
@@ -1,15 +1,12 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
-});
-
-$(document).ready(function(){
-    Materialize.toast('Upload with this button <i class="material-icons">fast_forward</i>', 4000)
-})
-
-$(document).ready(function(){
-    $('.modal-trigger').leanModal();
-});
-
-$(document).ready(function() {
     $('select').material_select();
+    $('.modal-trigger').leanModal();
+
+    if (top.location.href === 'http://localhost:63342/Vef-PHP/Website/' || top.location.href === 'http://localhost:63342/Vef-PHP/Website/index.php') {
+        Materialize.toast('Upload with this button <i class="material-icons">fast_forward</i>', 4000);
+    }
+
+    document.getElementById('here').style.backgroundColor = "rgba (255, 255 ,255 , 0.2)";
+    document.getElementById('here').style.color = "rgba (0, 0 ,0 , 1)";
 });
